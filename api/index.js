6ff9f6044f6d1fc4f16d10846bbbcbf017b776cc
AvlokitaPathania <avlokita.cs24@bmsce.ac.in>
2026-05-05 21:25:18 +0530

    backend fix

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
-const app = require("./server");
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import certificateRoutes from "./routes/certificate.js";
 
-module.exports = app;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/certificate", certificateRoutes);
+
+export default app;
